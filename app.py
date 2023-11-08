52e2c4c9e2e14ca5e17fc9c55faa8cafe6c559ec
fengb1 <fengb1@email.arizona.edu>
2023-11-08 15:16:20 -0800

    Updated memory feature for the bot

--- app.py
+++ app.py
@@ -1,45 +1,61 @@
-from langchain import OpenAI, LLMChain, PromptTemplate
-from langchain.memory import ConversationBufferWindowMemory
-from dotenv import find_dotenv, load_dotenv
-import requests
-#from playsound import playsound
 import os
+import openai
+from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-def get_response_from_ai(human_input):
-    template = """
+INSTRUCTIONS = """
     you are as a role of my teacher's assistant, now let's playing the following requirements:
     1/ you need to be nice to me, be encouraged, always say something nice to me
     2/ you need to answer my questions with as many details you can provide on the problem
     3/ you need to provide an example related to reality to me for every academic question i asked
     4/ don't be too boring, always be happy to help me
-
-    {history}
-    student: {human_input}
-    you:
     """
+TEMPERATURE = 0.5
+MAX_TOKENS = 500
+FREQUENCY_PENALTY = 0
+PRESENCE_PENALTY = 0.6
+MAX_CONTEXT_QUESTIONS = 10
 
-    prompt = PromptTemplate(
-        input_variables={"history", "human_input"},
-        template = template
+def get_response_from_ai(instructions, history, human_input):
+    """Get a response from ChatCompletion
+
+    Args:
+        instructions: The instructions for the chat bot - this determines how it will behave
+        history: Chat history
+        human_input: The new question to ask the bot
+
+    Returns:
+        The response text
+    """
+    # build the messages
+    messages = [
+        { "role": "system", "content": instructions },
+    ]
+    # add the previous questions and answers
+    for question, answer in history[-MAX_CONTEXT_QUESTIONS:]:
+        messages.append({ "role": "user", "content": question })
+        messages.append({ "role": "assistant", "content": answer })
+    # add the new question
+    messages.append({ "role": "user", "content": human_input })
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=TEMPERATURE,
+        max_tokens=MAX_TOKENS,
+        top_p=1,
+        frequency_penalty=FREQUENCY_PENALTY,
+        presence_penalty=PRESENCE_PENALTY,
     )
-
-    chatgpt_chain = LLMChain(
-        llm=OpenAI(temperature=0.2),
-        prompt=prompt,
-        verbose=True,
-        memory=ConversationBufferWindowMemory(k=2)
-    )
-
-    output = chatgpt_chain.predict(human_input=human_input)
-
-    return output
+    return completion.choices[0].message.content
 
 # Build web GUI
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+history = []
 
 @app.route("/")
 def home():
@@ -48,7 +64,8 @@ def home():
 @app.route('/send_message', methods=['POST'])
 def send_message():
     human_input=request.form['human_input']
-    message = get_response_from_ai(human_input)
+    message = get_response_from_ai(INSTRUCTIONS, history, human_input)
+    history.append((human_input, message))
     return message
 
 if __name__=="__main__":
